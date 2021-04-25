e7a402d0954625f8fd8e9db75b93ee87adc5dc99
Brian Balke <brian.balke@hotmail.com>
2021-04-25 08:20:01 -0700

    Use static class to provide design data.

---
+++ RoleAssigner/ViewModels/DesignData.cs
@@ -0,0 +1,104 @@
+﻿using HypnosisRising.CaseWork.Common;
+using HypnosisRising.CaseWork.Roles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HypnosisRising.RoleAssigner.ViewModels
+{
+    class DesignData
+    {
+        //***** PersonForm
+
+        public static Person Shaw =
+            new Person()
+            {
+                FirstName = "George",
+                LastName = "Shaw",
+                Nickname = "Jr",
+                Context = CaseWork.Common.Environment.Friendship,
+                Role = "Intellectual"
+            };
+
+        public static PersonFormViewModel ShawVM =
+            new PersonFormViewModel()
+            {
+                Person = Shaw
+            };
+
+        //***** ContactForm
+
+        public static Contact Headroom =
+            new Contact()
+            {
+                FirstName = "Max",
+                LastName = "Headroom",
+                Nickname = "Grazie",
+                Context = CaseWork.Common.Environment.Social,
+                Role = "Virtual Avatar",
+                EMail = "max@dead.edu",
+                Phone = "818-818-8188",
+                HasText = true
+            };
+
+        public static ContactFormViewModel HeadroomVM =
+            new ContactFormViewModel() { Contact = Headroom };
+
+        //***** ClientForm
+
+        public static Contact Fluffy =
+            new Contact()
+            {
+                FirstName = "Fluffy",
+                LastName = "Bird",
+                Nickname = "Pootie Pie",
+                Context = CaseWork.Common.Environment.Family,
+                Role = "Nestmate",
+                EMail = "fluffy@wb.com",
+                Phone = "818-444-8723",
+                HasText = false
+            };
+
+        public static Contact Sylvester =
+            new Contact()
+            {
+                FirstName = "Sylvester",
+                LastName = "Cat",
+                Nickname = "Putty",
+                Context = CaseWork.Common.Environment.Career,
+                Role = "Stalker",
+                EMail = "sylvester@wb.com",
+                Phone = "310-333-9023",
+                HasText = true
+            };
+
+        public static Location Cage =
+            new Location()
+            {
+                Street1 = "100 Acorn St",
+                Street2 = "Gilded Cage",
+                District = "New York",
+                Region = "NY",
+                MailCode = "09223",
+                Country = "USA"
+            };
+
+        public static Client Tweety =
+            new Client()
+            {
+                FirstName = "Tweey",
+                LastName = "Bird",
+                EMail = "tweety@sb.com",
+                Phone = Fluffy.Phone,
+                HasText = Fluffy.HasText,
+                DateOfBirth = DateTime.Parse("Aug 15,1957"),
+                HomeAddress = Cage,
+                Partner = Fluffy,
+                EmergencyContact = Sylvester,
+                BillingRate = 225
+            };
+
+        public static ClientFormViewModel TweetyVM =
+            new ClientFormViewModel() { Client = Tweety };
+    }
+}
